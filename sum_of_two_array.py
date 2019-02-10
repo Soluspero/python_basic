073b197b76fd72134d437546a5dd089b068408dd
soluspero <soluspero@gmail.com>
2019-02-10 12:34:32 +0530

    sum of two array

---
+++ sum_of_two_array.py
@@ -0,0 +1,22 @@
+
+def main():
+	n=4
+	a = [1,2,3,4]
+	b = [3,4,5,6]
+
+	#solution 1
+	# result = map(lambda x,y: x+y,a,b)
+
+	#solution 2
+	# result = [a for a in (x+y for x,y in zip(a,b))]
+	result  = [a[i]+b[i] for i in range(n)]
+
+	#solution 3
+	# result = []
+	# for i in range(4):
+	# 	result.append(a[i]+b[i])
+
+	print(result)
+
+if __name__ == '__main__':
+	main()
